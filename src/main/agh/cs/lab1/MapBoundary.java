017bccf5ec6b975c8e10c97c1296e3433eaaafe9
JanRaj00 <janrajczyk20@gmail.com>
2020-11-18 20:49:26 +0100

    Lab7

---
+++ src/main/agh/cs/lab1/MapBoundary.java
@@ -0,0 +1,36 @@
+package agh.cs.lab1;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+public class MapBoundary implements IPositionChangeObserver{
+    SortedSet<IMapElement> setX = new TreeSet<IMapElement>(new elementComparatorX());
+    SortedSet<IMapElement> setY = new TreeSet<IMapElement>(new elementComparatorY());
+
+    @Override
+    public void positionChanged(Vector2d oldPosition, Vector2d newPosition, Animal animal){
+        if(oldPosition.x!= newPosition.x) {
+            setX.remove(animal);
+            setX.add(animal);
+        }
+        if(oldPosition.y!= newPosition.y) {
+            setY.remove(animal);
+            setY.add(animal);
+        }
+    }
+
+    public void place(IMapElement element){
+        setX.add(element);
+        setY.add(element);
+        if(element instanceof Animal) {
+            ((Animal) element).addObserver(this);
+        }
+    }
+
+    public Vector2d getBoundaryLeft(){
+            return new Vector2d(setX.first().getPosition().x, setY.first().getPosition().y);
+    }
+
+    public Vector2d getBoundaryRight() {
+            return new Vector2d(setX.last().getPosition().x, setY.last().getPosition().y);
+    }
+}
---
+++ src/main/agh/cs/lab1/elementComparatorX.java
@@ -0,0 +1,14 @@
+package agh.cs.lab1;
+
+import java.util.Comparator;
+public class elementComparatorX implements Comparator <IMapElement> {
+    public int compare(IMapElement o1, IMapElement o2) {
+        Vector2d v1=o1.getPosition();
+        Vector2d v2=o2.getPosition();
+        if(v1.equals(v2))
+            return o1.getClass().getSimpleName().compareTo(o2.getClass().getSimpleName());
+        if(v1.x==v2.x)
+            return (v1.y-v2.y);
+        return (v1.x-v2.x);
+    }
+}
